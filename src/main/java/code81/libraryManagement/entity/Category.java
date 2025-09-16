ee0be9ff32fa94cd9445ddcb76a4e0344ac279ec
mahmoudalbanhawi <mahmoudalbanhawi863@gmail.com>
2025-09-16 21:02:10 +0300

    completing the main entities for library management project.

--- src/main/java/code81/libraryManagement/entity/Category.java
+++ src/main/java/code81/libraryManagement/entity/Category.java
@@ -1,4 +1,20 @@
 package code81.libraryManagement.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue
+    private long id ;
+    private String name ;
+    @ManyToOne
+    private Category parentCategory;
 }
--- src/main/java/code81/libraryManagement/entity/SystemUser.java
+++ src/main/java/code81/libraryManagement/entity/SystemUser.java
@@ -1,4 +1,21 @@
 package code81.libraryManagement.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class SystemUser {
+    @Id
+    @GeneratedValue
+    private Long id ;
+    @Column(unique = true)
+    private String userName ;
+    private String password ;
+    private String role;
 }
